7936d26a7363ae5647e6c27d2aab71f010f3dd25
谢擎天 <xieqingtian@xieqingtiandeMacBook-Pro.local>
2020-04-03 01:05:23 +0800

    999

--- src/renderer/app/App.tsx
+++ src/renderer/app/App.tsx
@@ -3,8 +3,8 @@ import { hot } from 'react-hot-loader/root';
 import { useSelector } from 'react-redux';
 
 import './App.scss';
+import { RootState } from '@/renderer/store/rootReducer';
 import Hello from '../features/Hello';
-import { RootState } from '@/store/rootReducer';
 
 function App() {
     const count = useSelector((state: RootState) => state.count.count);
