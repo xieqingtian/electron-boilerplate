02e76e28c0aabeb1ceaa43acf2357944fe8af5d4
xieqingtian <1255699673@qq.com>
2020-04-26 18:49:15 +0800

    666

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,10 @@ module.exports = {
         'no-plusplus': 0,
         'no-param-reassign': 0,
         'import/no-extraneous-dependencies': 0,
+        'func-names': 0,
+        'consistent-return': 0,
+        'no-await-in-loop': 0,
+        'no-continue': 0,
+        'no-restricted-syntax': 0,
     },
 };
--- qt.config.js
+++ qt.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     electron: {},
     sassResources: ['src/renderer/sass/vars.scss', 'src/renderer/sass/mixins.scss'],
+    publicPath: '',
 };
